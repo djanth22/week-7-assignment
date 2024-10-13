fcca379600ec9a499564a7b794909f7fdfd74edc
David Anthony <davywavy22@googlemail.com>
2024-10-13 20:02:40 +0100

    changed logo image

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,11 @@
 export default function Logo() {
   return (
     <>
-      <img src="/public/images/snake.png" alt="snake logo" className="snake" />
+      <img
+        src="https://th.bing.com/th/id/R.2f60385f617b9b7ce2c0a109760618b9?rik=zLxDTwLjXrfHmA&riu=http%3a%2f%2forig03.deviantart.net%2fc20e%2ff%2f2009%2f045%2f7%2f6%2fgolden_snake_by_janxu.png&ehk=ZisPzjuMtNpdSe9JNLVV8EAg0ZJbdkN17LozY3bCAos%3d&risl=&pid=ImgRaw&r=0"
+        alt="snake logo"
+        className="snake"
+      />
     </>
   );
 }
